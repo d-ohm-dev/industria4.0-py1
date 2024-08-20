f002d81b904937214227274259fc1ce3c19bd84c
Demian Ovejero <demian07@gmail.com>
2024-08-20 14:02:51 -0300

    adding Threading

--- turbinaSF.py
+++ turbinaSF.py
@@ -13,26 +13,46 @@ class Turbina():
         self.frenoEmergencia = False
         self.RPM = 0.0
         self.friccion = 20.0
-        self.aporteGas = 0.0
         self.presionGas = 0.0
         self.temperatura = 0.0 
         self.aporteMotor = 0.0
-        self.aporteQuemadores = 0.0
+        self.aporteQ = 0.0
         self.anteriores = []
+        
+    #Se intentan implementar hilos para correr un lazo PID de regulacion de temperatura y presion automatica en forma paralela sin éxito
+    # A CORREGIR
+        self.autoPID = threading.Event()
+
+        self.hiloTemp = threading.Thread(
+            target = self.PID,
+            args = (self.temperatura, True, 0.0, 200.0),
+            daemon=True
+        )
+
+        self.hiloPresion = threading.Thread(
+            target = self.PID,
+            args = (self.presionGas, True, 0.0, 4.5),
+            daemon=True
+        )
 
     def update(self):
+        
+
+        self.autoPID.set()
+    # Si la junta neumática esta en True y el motor auxiliar esta encendido, el aporte de aceleración es 100, de lo contrario se restablece a cero. 
         if self.motorAux and self.junta:
             self.aporteMotor = 100.0
         else:
             self.aporteMotor = 0.0
-
+    # Si ambos quemadores estan encendidos y la velocidad es mayor a 478, el aporte de aceleración sera proporcional a la apertura de la Válvula
+    # Se agrega la variable glas acá de forma provisoria para implementar la secuencia del freno de emergencia
         if self.Q1 and self.Q2 and self.RPM > 478.0: 
-            self.aporteQuemadores = self.Valvula * self.aporteGas
-            self.presionGas += 0.01
+            self.aporteQ = self.Valvula 
+            self.presionGas += 0.09
 
-        self.RPM += self.aporteMotor + self.aporteQuemadores - self.friccion
+        self.RPM += self.aporteMotor + self.aporteQ - self.friccion
 
-        self.temperatura += 10.0
+        self.temperatura += 1.0
         
         
         if self.freno > 0.0:
@@ -47,9 +67,24 @@ class Turbina():
             
         if self.RPM <= 0.0:
             self.RPM = 0.0
+    
+    # desactivar esta sección si se logra hacer funcionar el lazo PID con la regulación de la temeperatura y la presion, por ahora se
+    # agregan estas condicionales para evitar que se active el freno de emergencia por estas dos condiciones
+        if self.presionGas >= 5.5:
+            self.presionGas = 5.5
+        if self.temperatura >= 350.0:
+            self.temperatura = 350.0
         
-        self.autoPID = threading.Event()
-        self.autoPID.set()
+    # Secuencia de frenado de emergencia
+        if self.RPM > 5500.0 or self.presionGas > 5.5 or self.temperatura > 350.0:
+            self.frenoEmergencia = True
+            self.Valvula = 0.0
+
+        if self.RPM > 4000.0:
+            if self.presionGas < 3.3:
+                time.sleep(3.0)
+                self.frenoEmergencia = True
+                self.Valvula = 0.0
 
     def PID(self,input, Man_Auto = False, SetpointMan = 0.0, SetpointAuto = 0.0):
             """
@@ -118,20 +153,10 @@ class Turbina():
 
 TUR = Turbina()
 estado = 1
-#while TUR.autoPID.is_set():
-    #hiloTemp = threading.Thread(
-        #target = TUR.PID,
-        #args = (TUR.temperatura, True, 0.0, 300.0),
-        #daemon=True
-    #)
-#while TUR.autoPID.is_set():
-    #hiloPresion = threading.Thread(
-        #target = TUR.PID,
-        #args = (TUR.presionGas, True, 0.0, 4.5),
-        #daemon=True
-    #)
-    ##hiloPresion.start()
-    #hiloTemp.start()
+TUR.update()
+if TUR.autoPID.is_set():
+    TUR.hiloTemp.start()
+    TUR.hiloPresion.start()
 
 try:
     while True:
@@ -145,11 +170,11 @@ try:
             TUR.Q1 = True
             TUR.Q2 = True
             time.sleep(2.0)
-            TUR.aporteGas = 10.0
+            TUR.Valvula = 10.0
             if TUR.Q1 and TUR.Q2:
                 estado += 1
         elif estado == 3:
-            TUR.aporteGas = 25.0
+            TUR.Valvula = 25.0
             if TUR.RPM > 2750.0:
                 TUR.motorAux = False
                 time.sleep(3.0)
@@ -157,25 +182,28 @@ try:
                 estado += 1
         elif estado == 4:
             TUR.PID(input=TUR.RPM, Man_Auto = False, SetpointAuto=4600.0)
-            if TUR.RPM == 4600.0:
-                estado += 1
-                TUR.PID(input=TUR.Valvula, Man_Auto = True, SetpointMan=10.0)
-        elif estado == 5:
-            TUR.aporteGas = 0.0
+
+        if estado == 5:
+            TUR.PID(input=TUR.Valvula, Man_Auto = True, SetpointMan=10.0)
             time.sleep(0.2)
+            TUR.Valvula = 0.0
             TUR.Q1 = False
             TUR.Q2 = False
             if TUR.RPM <=2500.0:
                 TUR.freno = 100.0
-        
-        if TUR.RPM > 5500.0 or TUR.presionGas > 5.5 or TUR.temperatura > 350.0:
-            TUR.frenoEmergencia = True
-            TUR.Valvula = 0.0
+
+        if TUR.freno == True:
+            estado = "Frenado manual"
+
+        if TUR.frenoEmergencia == True:
             estado = "Frenado de Emergencia"
+
         TUR.update()
         print(f"Presion: {TUR.presionGas:.2f}, Temp: {TUR.temperatura:.2f}")
         print(f"VEL: {TUR.RPM:.2f}, VALV: {TUR.Valvula:.2f}, Estado {estado}")
+        #print(f"autoPID: {TUR.autoPID.is_set()}, hiloPresion: {TUR.hiloPresion.is_alive()}, hiloTemp: {TUR.hiloTemp.is_alive()}")
         time.sleep(0.1)
+    
 except KeyboardInterrupt:
     print("Simulación finalizada")
-    #TUR.autoPID.clear()
+    TUR.autoPID.clear()
